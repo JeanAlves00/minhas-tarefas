1b79e8a674ff0a7facc8579cd6fe148acb83f8db
Jean <154983968+JeanAlves00@users.noreply.github.com>
2024-12-18 15:03:16 -0400

    0.1

---
+++ src/containers/ListaDeTarefa/index.tsx
@@ -0,0 +1,9 @@
+const listaDeTarefas = () => (
+  <main>
+    <p>
+      2 tarefas marcadadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;
+    </p>
+  </main>
+)
+
+export default listaDeTarefas
